8e6a0a368fb11df5436c1258379e474c40471497
zoomdong <1344492820@qq.com>
2021-10-05 00:10:54 +0800

    feat: complete guess game

--- demo/guess_game/src/main.rs
+++ demo/guess_game/src/main.rs
@@ -4,18 +4,22 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("guess");
-    println!("Input: ");
 
     let secret_num = rand::thread_rng().gen_range(1, 101);
 
-    println!("Secret Num is {}", secret_num);
+    // println!("Secret Num is {}", secret_num);
 
     loop {
+        println!("Input: ");
         let mut guess = String::new();
         io::stdin().read_line(&mut guess).expect("Fail read");
         // 需要将 string 转成 num
         // rust 允许利用同名的变量来替换旧变量的值，通常出现在类型转换的场景
-        let guess: u32 = guess.trim().parse().expect("Type a num");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            // 处理非法输入跳出
+            Err(_) => continue,
+        };
     
         println!("You guess is {}", guess);
     
