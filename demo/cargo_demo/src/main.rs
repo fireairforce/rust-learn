e1c5896b2c9b6e495fc40314d8559868bc342730
zoomdong <1344492820@qq.com>
2021-10-06 00:45:33 +0800

    feat: update demo

--- demo/cargo_demo/src/main.rs
+++ demo/cargo_demo/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_len(s1);
+
+    println!("length of {} is {}", s2, len);
+}
+
+fn calculate_len(s: String) -> (String, usize) {
+    let length = s.len();
+
+    (s, length)
 }
