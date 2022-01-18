e09d44b9747383c3f6a3e0b0a41a37550226a063
zoomdong <1344492820@qq.com>
2022-01-19 00:57:32 +0800

    feat: add post and get

--- demo/httpie/src/main.rs
+++ demo/httpie/src/main.rs
@@ -1,7 +1,16 @@
+// 命令行参数解析
 use clap::Parser;
+// 校验字段是否合法
 use anyhow:: { Result, anyhow };
-use reqwest::Url;
-use std::str::FromStr;
+use reqwest::{
+  Url,
+  Client,
+  header
+};
+use std::{ 
+  str::FromStr,
+  collections::HashMap
+};
 
 #[derive(Parser, Debug)]
 #[clap(version = "1.0", author = "zoomdong")]
@@ -65,9 +74,70 @@ fn parse_url(s: &str) -> Result<String> {
   Ok(s.into())
 }
 
-// Clap 提供了宏使得捕获 cli 输入简单
-fn main () {
-  let opts: Opts = Opts::parse();
+// 处理 get 子命令
+async fn get(clinet: Client, args: &Get) -> Result<()> {
+  let resp = clinet.get(&args.url).send().await?;
+  println!("{:?}", resp.text().await?);
+  Ok(())
+}
 
-  println!("{:?}", opts);
+// 处理 post 子命令
+async fn post(clinet: Client, args: &Post) -> Result<()> {
+  let mut body = HashMap::new();
+  for pair in args.body.iter() {
+    body.insert(&pair.k, &pair.v);
+  }
+  let resp = clinet.post(&args.url).json(&body).send().await?;
+  println!("{:?}", resp.text().await?);
+  Ok(())
+}
+
+// Clap 提供了宏使得捕获 cli 输入简单
+#[tokio::main]
+async fn main () -> Result<()> {
+  let opts: Opts = Opts::parse();
+  // http 客户端生成
+  let mut headers = header::HeaderMap::new();
+  // 为 http 客户端补一些缺省的 http 头
+  headers.insert("X-POWERED-BY", "Rust".parse()?);
+  headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
+  let client = reqwest::Client::builder().default_headers(headers).build()?;
+  let result = match opts.subcmd {
+    SubCommand::Get(ref args) => get(client, args).await?,
+    SubCommand::Post(ref args) => post(client, args).await?,
+  };
+
+  Ok(result)
+}
+
+// 编写一下测试, cfg test 表示整个 mod tests 只在 cargo test 的时候才编译
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn parse_url_works() {
+    assert!(parse_url("abc").is_err());
+    assert!(parse_url("http://abc.xyz").is_ok());
+    assert!(parse_url("https://httpbin.org/post").is_ok());
+  }
+
+  #[test]
+  fn parse_kv_pair_works() {
+    assert!(parse_kv_pair("a").is_err());
+    assert_eq!(
+      parse_kv_pair("a=1").unwrap(),
+      KvPair {
+        k: "a".into(),
+        v: "1".into()
+      }
+    );
+    assert_eq!(
+      parse_kv_pair("b=").unwrap(),
+      KvPair {
+        k: "b".into(),
+        v: "".into()
+      }
+    );
+  }
 }
