87bbb409a9ef545dc5afda75141f1deb26bf22f5
zoomdong <1344492820@qq.com>
2022-02-14 09:32:37 +0800

    chore: add borrow

---
+++ packages/cargo_demo/src/borrow1.rs
@@ -0,0 +1,7 @@
+fn main() {
+  let mut data: Vec<&u32> = Vec::new();
+  let v = 42;
+  data.push(&v);
+  println!("data: {:?}", data);
+}
+
---
+++ packages/cargo_demo/src/borrow2.rs
@@ -0,0 +1,11 @@
+
+fn main() {
+  let mut data: Vec<&u32> = Vec::new();
+  push_local_ref(&mut data);
+  println!("data: {:?}", data);
+}
+
+fn push_local_ref(data: &mut Vec<&u32>) {
+  let v = 42;
+  data.push(&v);
+}
